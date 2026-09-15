2813374de574c3820d11da3b7ae6052338542481
Hiếu Trần <hieu07700@gmail.com>
2026-09-15 16:14:36 +0700

    feat(Todo 2.2): Add mapping Department into Employee

--- Slot3/Chapter2-JPA-CRUDMapping/src/main/java/fu/de200110/pojo/Employee.java
+++ Slot3/Chapter2-JPA-CRUDMapping/src/main/java/fu/de200110/pojo/Employee.java
@@ -32,6 +32,10 @@ public class Employee {
 
     private boolean active;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id", nullable = false)
+    private Department department;
+
     public Employee() {
     }
 
@@ -107,5 +111,13 @@ public class Employee {
 
     public void setActive(boolean active) {
         this.active = active;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
     }
 }
